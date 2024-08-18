945a479cb91c50b4a1a653e03f26e5eb4780b110
Pavan Medi <pamedi@ibasis.net>
2024-08-18 14:11:47 +0530

    mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'opsguruu',
+        'USER': 'opsguruu',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
